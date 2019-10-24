3a194c8512563dfbe42f20455b43e1fde7b8050d
Justin-sd <jggross@ucsd.edu>
2019-10-24 15:55:14 -0700

    Clean up boilerplate a bit and register Event model to django admin

--- LinkUp/core/models.py
+++ LinkUp/core/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-# Create your models here.
 
 class Event(models.Model): 
-	'''
+	"""
 	title - The title of the event
-	'''
+	"""
+	title = models.CharField(max_length=250)
 
-	title = models.CharField(max_length = 250)
-
-	
