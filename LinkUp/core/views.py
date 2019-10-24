e74fb3eb61a6051807d6a03eff1e2f2166381289
Kevin Rafferty <steelerkevin7@yahoo.com>
2019-10-24 02:11:44 -0700

    BootStrap Version

--- LinkUp/core/views.py
+++ LinkUp/core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-	return render(request, "core/index.html", {}) 
 
+def home(request):
+	return render(request, "core/homepage.html", {})
+
+
+def event_page(request):
+	return render(request, "core/event_page.html", {})
