3f709765955b3acfa7aae9adfa7d2d463123fd4c
Justin-sd <jggross@ucsd.edu>
2019-10-27 10:39:48 -0700

    Define models

--- LinkUp/core/admin.py
+++ LinkUp/core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Event
+from .models import *
 
 
+admin.site.register(Availability)
+admin.site.register(Schedule)
+admin.site.register(LinkUpUser)
 admin.site.register(Event)
--- LinkUp/core/models.py
+++ LinkUp/core/models.py
@@ -1,11 +1,33 @@
 from django.db import models
 
 
+class Availability(models.Model):
+	availability = models.CharField(max_length=1500)
+
+
+class LinkUpUser(models.Model):
+	email = models.EmailField(primary_key=True)
+	predefinedSchedule = models.ForeignKey(Availability, on_delete=models.CASCADE, blank=True, null=True)
+	user_name = models.CharField(max_length=30)
+	password = models.CharField(max_length=50)
+
+
+class Schedule(Availability):
+	user = models.ForeignKey(LinkUpUser, on_delete=models.CASCADE)
+
+
 class Event(models.Model): 
 	"""
 	title - The title of the event
 	"""
 	event_id = models.CharField(max_length=32, primary_key=True)
+	owner = models.ForeignKey(LinkUpUser, on_delete=models.CASCADE, related_name='event_owner')
+	admins = models.ManyToManyField(LinkUpUser, related_name='event_admins')
+	members = models.ManyToManyField(LinkUpUser, related_name='event_members')
 	title = models.CharField(max_length=250)
 	description = models.CharField(max_length=250, null=True)
-
+	duration = models.DecimalField(decimal_places=2, max_digits=12, null=True)
+	potentialStartDate = models.DateTimeField()
+	potentialEndDate = models.DateTimeField()
+	finalStartDate = models.DateTimeField(blank=True, null=True)
+	finalEndDate = models.DateTimeField(blank=True, null=True)
