5c0b34527b87e2e55da0b17c0679dace2cfb533a
Eric Luong <erluong@ucsd.edu>
2019-11-22 11:27:54 -0800

    Merge branch 'production' of https://github.com/Justin-sd/LinkUp into production
    # Conflicts:
    #	LinkUp/core/models.py
    #	LinkUp/core/views.py

--- LinkUp/core/models.py
+++ LinkUp/core/models.py
@@ -3,27 +3,36 @@ from django.contrib.auth.models import User
 
 
 class Event(models.Model):
-	"""
-	title - The title of the event
-	"""
-	event_id = models.CharField(max_length=32, primary_key=True)
-	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_owner')
-	admins = models.ManyToManyField(User, related_name='event_admins')
-	members = models.ManyToManyField(User, related_name='event_members')
-	title = models.CharField(max_length=250)
-	description = models.CharField(max_length=250, null=True)
-	duration = models.DecimalField(decimal_places=2, max_digits=12, null=True)
-	potentialStartDate = models.DateTimeField()
-	potentialEndDate = models.DateTimeField()
-	finalStartDate = models.DateTimeField(blank=True, null=True)
-	finalEndDate = models.DateTimeField(blank=True, null=True)
+    """
+    title - The title of the event
+    """
+    event_id = models.CharField(max_length=32, primary_key=True, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_owner', null=True)
+    admins = models.ManyToManyField(User, related_name='event_admins')
+    members = models.ManyToManyField(User, related_name='event_members')
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, null=True)
+    duration = models.DecimalField(decimal_places=2, max_digits=12, null=True)
+    potential_start_date = models.DateTimeField()
+    potential_end_date = models.DateTimeField()
+    final_start_date = models.DateTimeField(blank=True, null=True)
+    final_end_date = models.DateTimeField(blank=True, null=True)
 
 
 class Schedule(models.Model):
-	availability = models.CharField(max_length=50000)
-	user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    availability = models.CharField(max_length=50000)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
-class EventSchedule(Schedule):
-	times = models.CharField(max_length=50000)
-	event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+class EventSchedule(models.Model):
+    availability = models.CharField(max_length=50000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'event')
+
+
+class UserTimezone(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    timezone_str = models.CharField(max_length=50)
