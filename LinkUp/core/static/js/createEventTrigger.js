1137e903dfbf6564cebe0d424b67237a01982ce2
Justin <jggross@ucsd.edu>
2019-11-24 10:16:37 -0800

    Fix logout button, import from google calendar button on all pages, main create event button, and some js console errors

--- LinkUp/core/static/js/createEventTrigger.js
+++ LinkUp/core/static/js/createEventTrigger.js
@@ -1,59 +1,57 @@
-$(document).ready(function() {
-    // Get the modal
-    const modal = document.getElementById("createEventModal");
+// Get the modal
+const modal = document.getElementById("createEventModal");
 
-    // Get the button that opens the modal
-    const btnCreateEventModal = document.getElementById("btn-open-create-event-modal");
+// Get the button that opens the modal
+const btnCreateEventModal = document.getElementById("btn-open-create-event-modal");
 
-    // Get the <span> element that closes the modal
-    const span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks the button, open the modal
-    btnCreateEventModal.onclick = function () {
-        renderCreateEventForm();
-        modal.style.display = "block";
-    };
+// When the user clicks the button, open the modal
+btnCreateEventModal.onclick = function () {
+    renderCreateEventForm();
+    modal.style.display = "block";
+};
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target === modal) {
         modal.style.display = "none";
-    };
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    };
-});
+    }
+};
 
 function renderCreateEventForm() {
-        $.ajax({
-            type: "GET",
-            url: "/create_event_form/",
-        }).success(function (form_content) {
-            $("#create-event-body").html(form_content);
-        }).fail(function () {
-            alert("Failed to load the event form");
-        });
-    }
+    $.ajax({
+        type: "GET",
+        url: "/create_event_form/",
+    }).success(function (form_content) {
+        $("#create-event-body").html(form_content);
+    }).fail(function () {
+        alert("Failed to load the event form");
+    });
+}
 
 function validateCreateEventForm() {
-        const eventDuration = $("#id_duration").val();
-        const eventStart = Date.parse($("#id_potential_start_date").val());
-        const eventEnd = Date.parse($("#id_potential_end_date").val());
+    const eventDuration = $("#id_duration").val();
+    const eventStart = Date.parse($("#id_potential_start_date").val());
+    const eventEnd = Date.parse($("#id_potential_end_date").val());
 
-        let validationStatus = true;
-        if (eventStart >= eventEnd) {
-            $("#id_potential_start_date").after("<div><p class='has-text-danger'>Potential start date must be before potential end date.</p></div>");
-            validationStatus = false;
-        }
-        if (eventDuration < 0) {
-            $("#id_duration").after("<div><p class='has-text-danger'>Duration must be greater than 0 minutes.</p></div>");
-            validationStatus = false;
-        }
-        if (validationStatus) {
-            modal.style.display = "none";
-        }
-        return validationStatus;
+    let validationStatus = true;
+    if (eventStart >= eventEnd) {
+        $("#id_potential_start_date").after("<div><p class='has-text-danger'>Potential start date must be before potential end date.</p></div>");
+        validationStatus = false;
     }
+    if (eventDuration < 0) {
+        $("#id_duration").after("<div><p class='has-text-danger'>Duration must be greater than 0 minutes.</p></div>");
+        validationStatus = false;
+    }
+    if (validationStatus) {
+        modal.style.display = "none";
+    }
+    return validationStatus;
+}
--- LinkUp/core/static/js/logoutModal.js
+++ LinkUp/core/static/js/logoutModal.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-  const modal2 = document.getElementById("LogOutModal");
-
-  // Get the button that opens the modal
-  const btn2 = document.getElementById("logoutbutton");
-
-  // When the user clicks the button, open the modal
-  btn2.onclick = function () {
-    modal2.show();
-  };
+$("#logout-button").on('click', function () {
+  $("#log-out-modal").show();
 });
