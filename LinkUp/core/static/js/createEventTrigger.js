10b9e89c61d62ba6297c6fb97728c6c4b36555fb
Justin <jggross@ucsd.edu>
2019-11-29 12:23:41 -0800

    Stop adding duplicate validation messages on invalid create event forms

--- LinkUp/core/static/js/createEventTrigger.js
+++ LinkUp/core/static/js/createEventTrigger.js
@@ -1,16 +1,7 @@
 
 function validateCreateEventForm() {
-
-    /*
-    let eventStartTime = new Date($("#id_no_earlier_than").val());
-    let eventEndTime = $("#id_no_later_than").val();
-    const eventStartTimeHour = eventStartTime.getHour();
-    const eventEndTimeHour = eventEndTime.getHours();
-    const eventStartTimeMin = eventStart.getMinutes();
-    const eventEndTimeMin = eventEndDate.getMinutes();
-    eventStartTime = eventStartTimeHour + eventStartTimeMin;
-    eventEndTime = eventEndTimeHour + eventEndTimeMin;
-     */
+    // Remove old validation messages
+    $(".create_event_validation").remove();
 
     //start and end times as DATE object
     const eventStartDate = new Date($("#id_potential_start_date").val());
@@ -33,36 +24,36 @@ function validateCreateEventForm() {
 
     let validationStatus = true;
     if (eventStartMil > eventEndMil) {
-        $("#id_potential_start_date").after("<div><p class='has-text-danger'>Potential start date must be before potential end date.</p></div>");
+        $("#id_potential_start_date").after("<div class='create_event_validation'><p class='has-text-danger'>Potential start date must be before potential end date.</p></div>");
         validationStatus = false;
     }
 
     //Validating potential starting date
     if (eventStartDate.getMonth() < todaysDate.getMonth()) {
-        $("#id_potential_start_date").after("<div><p class='has-text-danger'>Potential start date must be in the future.</p></div>");
+        $("#id_potential_start_date").after("<div class='create_event_validation'><p class='has-text-danger'>Potential start date must be in the future.</p></div>");
         validationStatus = false;
     }
     else if ((eventStartDate.getMonth() === todaysDate.getMonth()) && (eventStartDate.getDate() < todaysDate.getDate() - 1)) {
-        $("#id_potential_start_date").after("<div><p class='has-text-danger'>Potential start date must be in the future.</p></div>");
+        $("#id_potential_start_date").after("<div class='create_event_validation'><p class='has-text-danger'>Potential start date must be in the future.</p></div>");
         validationStatus = false;
     }
 
     //Validating potential ending date
     if (eventEndDate.getMonth() < todaysDate.getMonth()) {
-        $("#id_potential_end_date").after("<div><p class='has-text-danger'>Potential end date must be in the future</p></div>");
+        $("#id_potential_end_date").after("<div class='create_event_validation'><p class='has-text-danger'>Potential end date must be in the future</p></div>");
         validationStatus = false;
     }
     else if ((eventEndDate.getMonth() === todaysDate.getMonth()) && (eventEndDate.getDate() < todaysDate.getDate() - 1)) {
-        $("#id_potential_end_date").after("<div><p class='has-text-danger'>Potential end date must be in the future.</p></div>");
+        $("#id_potential_end_date").after("<div class='create_event_validation'><p class='has-text-danger'>Potential end date must be in the future.</p></div>");
         validationStatus = false;
     }
 
     if (eventDuration <= 0) {
-        $("#id_duration").after("<div><p class='has-text-danger'>Duration must be greater than 0 minutes.</p></div>");
+        $("#id_duration").after("<div class='create_event_validation'><p class='has-text-danger'>Duration must be greater than 0 minutes.</p></div>");
         validationStatus = false;
     }
     if (eventEndTime - eventStartTime < 1) {
-            $("#id_no_earlier_than").after("<div><p class='has-text-danger'>Must be 1 hour or more between start and end time</p></div>");
+            $("#id_no_earlier_than").after("<div class='create_event_validation'><p class='has-text-danger'>Must be 1 hour or more between start and end time</p></div>");
             validationStatus = false;
     }
     if (validationStatus) {
