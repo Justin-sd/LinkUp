376ca91b6e8d645997bb3959d4ab8c43e312afe2
Justin <jggross@ucsd.edu>
2019-11-30 11:52:35 -0800

    collectstatic

---
+++ LinkUp/staticfiles/js/updateUser.js
@@ -0,0 +1,7 @@
+//Ignore this code for now
+function updateUser() {
+    const firstName = document.getElementById("first_name" ).value;
+    const lastName = document.getElementById("last_name").value;
+    const email = document.getElementById("email").value;
+    const updateUser = document.getElementById("updateUser").value;
+}
